d0a7da5ce42b2135aff24bf6cc610c0da6807e46
Iryna S <irynaradkovska@gmail.com>
2022-01-22 07:14:54 +0200

    Updates the 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join(``);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`)
+    .show();
+});
