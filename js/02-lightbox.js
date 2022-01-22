c505ca20b77e984a2775bf963b32d4fc73679f9d
Iryna S <irynaradkovska@gmail.com>
2022-01-22 08:52:25 +0200

    Added a markup to the 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join(``);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox(galleryContainer, {});
+
+console.log(lightbox);
+
